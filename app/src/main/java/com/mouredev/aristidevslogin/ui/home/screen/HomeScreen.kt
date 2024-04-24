d37abe2553e361b267ff17b1389153e962df302b
oscariglesiasmendez <iglesiasmendezoscar@gmail.com>
2024-04-24 22:18:41 +0200

    Menu drawer

--- app/src/main/java/com/mouredev/aristidevslogin/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/mouredev/aristidevslogin/ui/home/screen/HomeScreen.kt
@@ -12,11 +12,24 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,16 +38,55 @@ import androidx.compose.ui.unit.dp
 import com.mouredev.aristidevslogin.components.ProductCard
 import com.mouredev.aristidevslogin.ui.home.ui.ProductsViewModel
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 
-@SuppressLint("CoroutineCreationDuringComposition")
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("CoroutineCreationDuringComposition", "UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun HomeScreen(viewModel:ProductsViewModel) {
+fun HomeScreen(viewModel: ProductsViewModel) {
+
+
+
+
 
     Surface(
         color = Color(204, 173, 228),
         modifier = Modifier.fillMaxSize()
     ) {
+
+        //Estado inicial del menú lateral
+        val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+        val scope = rememberCoroutineScope()
+        ModalNavigationDrawer(
+            drawerContent = {
+                //Contenido del menú (items)
+                ModalDrawerSheet {
+                }
+            },
+            drawerState = drawerState
+        ) {
+            Scaffold() {
+                TopAppBar(
+                    title = {
+                        Text(text = "Mediaflix App")
+                    },
+                    navigationIcon = {
+                        IconButton(
+                            onClick = { scope.launch { drawerState.open() } }
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Menu,
+                                contentDescription = "Menu"
+                            )
+                        }
+                    }
+                )
+            }
+        }
+
+
         val productList = viewModel.products.collectAsState().value
         val context = LocalContext.current
         println(productList)
@@ -81,8 +133,8 @@ fun HomeScreen(viewModel:ProductsViewModel) {
             }
         }
     }
-
 }
+
 
 /*
 @Composable
